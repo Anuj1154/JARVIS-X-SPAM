6b9f391c8eea440ec4f3ffbf8a5c688a82cb4c05
Anuj1154 <158982341+Anuj1154@users.noreply.github.com>
2024-03-18 16:45:08 +0530

    Update start.py

--- JARVIS/modules/start.py
+++ JARVIS/modules/start.py
@@ -8,8 +8,8 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/JARVIS_X_SUPPORT"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/CHATTING_2024")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/monster_discussion"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/monster_king_is_here")
     ],
     [
         Button.url("• ʀᴇᴘᴏ •", "https://github.com/doraemon890/JARVIS-X-SPAM")
@@ -34,13 +34,13 @@ async def start(event):
         bot_name = ANNIE.first_name
         bot_id = ANNIE.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [JARVIS](https://t.me/JARVIS_V2)**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [MONSTER](https://t.me/MONSTER_TERA_BAAP1)**\n\n"
         TEXT += f"» **ᴊᴀʀᴠɪs V2 :** `M 1.8.31`\n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
               event.chat_id,
-                    "https://telegra.ph/file/ee9a464bb8c1a5277f6c3.jpg",
+                    "https://graph.org/file/38343b1ece04117d60ac8.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
                 )
